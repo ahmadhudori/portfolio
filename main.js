0b97ff42ca8f28044be5be6d1cb452435fb084f3
Ahmad Hudori <ahmadhudori06@gmail.com>
2023-11-02 12:42:56 +0700

    completing contact

--- main.js
+++ main.js
@@ -15,3 +15,26 @@ function toggleDetail(e) {
     detail.slideToggle();
 
 }
+
+function onFormSubmit(e) {
+    e.preventDefault()
+
+    const email = $("#email")
+    const subject = $("#subject")
+    const message = $("#message")
+
+    // console.log($(email).val())
+
+    if(!$(email).val()) {
+        alert("email is required")
+    } else if(!$(subject).val()) {
+        alert("subject is required")
+    } else if(!$(message).val()) {
+        alert("message is required")
+    } else {
+        alert("Submit succes")
+        $(email).val("")
+        $(subject).val("")
+        $(message).val("")
+    }
+};
